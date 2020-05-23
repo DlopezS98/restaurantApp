6e7dc37aacb5fddf1e13c5ffc843445b6c905aa9
Danny Lopez <01dlopezS98@gmail.com>
2020-05-22 18:44:17 -0600

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,6 @@
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
+import React, { useState } from "react";
 import {
   Image,
-  Platform,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -10,51 +8,117 @@ import {
   TextInput,
   ImageBackground,
   FlatList,
+  Alert,
+  Modal,
+  TouchableHighlight,
 } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-
-import { MonoText } from "../components/StyledText";
+import { LinearGradient } from "expo-linear-gradient";
 
 const popularData = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     image: require("../assets/images/BurgerPapas.jpg"),
     price: "$8.20",
-    description: "Combo"
+    description: "Combo",
   },
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     image: require("../assets/images/Sandwich.jpg"),
     price: "$10.5",
-    description: "Sandwich"
+    description: "Sandwich",
   },
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
     image: require("../assets/images/comboAlas_papas.jpg"),
     price: "$8.00",
-    description: "Combo"
+    description: "Combo",
   },
 ];
 
-const categoryData = [{
-  id:1,
-  categoryName:'Comida Rápida'
-},{
-  id:2,
-  categoryName:'Ensaladas'
-},{
-  id:3,
-  categoryName:'Gourmet'
-}];
+const bestFood = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    image: require("../assets/images/cmbPolloEnsalada.png"),
+    price: "$15.99",
+    description: "Ensalada + Pollo",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    image: require("../assets/images/comidaPollo.jpg"),
+    price: "$15.99",
+    description: "Ensalada + Pollo",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    image: require("../assets/images/EnsaladaCmb.jpg"),
+    price: "$15.99",
+    description: "Pollo",
+  },
+];
 
-function ItemMorePopular({image, price, desc }) {
+const categoryData = [
+  {
+    id: "1dlopes981",
+    categoryName: "Comida Rápida",
+  },
+  {
+    id: "1dlopes982",
+    categoryName: "Ensaladas",
+  },
+  {
+    id: "1dlopes983",
+    categoryName: "Gourmet",
+  },
+  {
+    id: "1dlopes984",
+    categoryName: "Cenas",
+  },
+  {
+    id: "1dlopes9815",
+    categoryName: "Desayunos",
+  },
+  {
+    id: "1dlopes986",
+    categoryName: "Almuerzos",
+  },
+];
+
+function ItemMorePopular({ image, price, desc }) {
   return (
     <TouchableOpacity style={styles.item}>
       <Image source={image} style={styles.imgItem} />
       <View style={styles.descItem}>
         <Text style={{ fontSize: 15 }}>{price}</Text>
-        <Text style={{fontSize: 15}}>{desc}</Text>
+        <Text style={{ fontSize: 15 }}>{desc}</Text>
       </View>
+    </TouchableOpacity>
+  );
+}
+
+function _bestFood({ image, price, desc }) {
+  return (
+    <TouchableOpacity style={styles.item}>
+      <Image source={image} style={styles.imgItem} />
+      <View style={styles.descItem}>
+        <Text style={{ fontSize: 15 }}>{price}</Text>
+        <Text style={{ fontSize: 15 }}>{desc}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+function ItemCategory({ category }) {
+  return (
+    <TouchableOpacity>
+      <LinearGradient
+        start={[0.2, 0.8]}
+        end={{ x: 1, y: 0 }}
+        colors={["#FF926B", "#FFC38B"]}
+        style={styles.itemCategory} 
+      >
+        <Text style={{ color: "#fff" }}>{category}</Text>
+      </LinearGradient>
     </TouchableOpacity>
   );
 }
@@ -65,7 +129,10 @@ export default function HomeScreen() {
       <View style={styles.container}>
         <View style={styles.header}>
           <TextInput placeholder="Buscar" style={styles.textInput} />
-          <ImageBackground
+          <LinearGradient
+            start={[1, 0.3]}
+            end={{ x: 0, y: 1 }}
+            colors={["#fbd072","#f2a365", "#282828"]}
             style={styles.banner}
             source={require("../assets/images/bannerHome.jpg")}
           >
@@ -85,14 +152,24 @@ export default function HomeScreen() {
                 source={require("../assets/images/foodFideos.png")}
               />
             </View>
-          </ImageBackground>
+          </LinearGradient>
         </View>
 
         <View style={styles.body}>
-        <View style={{paddingHorizontal:24, marginBottom:10}}>
+          <View style={{ paddingHorizontal: 24, marginBottom: 10 }}>
             <Text style={styles.title}>Categorias</Text>
           </View>
-          <View style={{paddingHorizontal:24, marginBottom:10}}>
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            collapsable={true}
+            horizontal
+            data={categoryData}
+            renderItem={({ item }) => (
+              <ItemCategory category={item.categoryName} />
+            )}
+            keyExtractor={(item) => item.id}
+          />
+          <View style={{ paddingHorizontal: 24, marginBottom: 10 }}>
             <Text style={styles.title}>Mas popular</Text>
           </View>
           <FlatList
@@ -101,7 +178,31 @@ export default function HomeScreen() {
             horizontal
             data={popularData}
             renderItem={({ item }) => (
-              <ItemMorePopular price={item.price} desc={item.description} image={item.image} />
+              <ItemMorePopular
+                price={item.price}
+                desc={item.description}
+                image={item.image}
+              />
+            )}
+            keyExtractor={(item) => item.id}
+          />
+          <View
+            style={{ paddingHorizontal: 24, marginBottom: 10, marginTop: 20 }}
+          >
+            <Text style={styles.title}>Mejores platillos</Text>
+          </View>
+          <FlatList
+            style={{ marginBottom: 60 }}
+            showsHorizontalScrollIndicator={false}
+            collapsable={true}
+            horizontal
+            data={bestFood}
+            renderItem={({ item }) => (
+              <_bestFood
+                image={item.image}
+                price={item.price}
+                desc={item.description}
+              />
             )}
             keyExtractor={(item) => item.id}
           />
@@ -134,9 +235,9 @@ const styles = StyleSheet.create({
   },
   banner: {
     height: 120,
-    resizeMode: "cover",
     padding: 10,
     justifyContent: "center",
+    borderRadius: 10,
   },
   body: {
     flex: 2,
@@ -148,8 +249,8 @@ const styles = StyleSheet.create({
     width: 200,
     marginVertical: 5,
     marginHorizontal: 10,
-    borderRadius:12,
-    elevation:3
+    borderRadius: 12,
+    elevation: 3,
   },
   imgItem: {
     width: "100%",
@@ -157,12 +258,18 @@ const styles = StyleSheet.create({
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
   },
-  descItem:{
-    backgroundColor:'#eee',
-    padding:5,
+  descItem: {
+    backgroundColor: "#eee",
+    padding: 5,
     borderBottomLeftRadius: 12,
     borderBottomRightRadius: 12,
-    flexDirection:'row',
-    justifyContent:'space-between'
-  }
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  itemCategory: {
+    padding: 10,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
 });
